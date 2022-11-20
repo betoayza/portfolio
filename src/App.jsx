fa47eb31782eec3719560b84eb78c14891e08341
betoayza <aaayza@gmail.com>
2022-11-19 22:43:42 -0300

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import * as Components from "./components/indexComponents";
 import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App" style={{ minHeight: "100vh" }}>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Components.Home />} />
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Components.Home />} />
 
-          <Route exact path="/contact" element={<Components.Contact />} />
+        <Route exact path="/contact" element={<Components.Contact />} />
 
-          <Route exact path="/about" element={<Components.About />} />
+        <Route exact path="/about" element={<Components.About />} />
 
-          <Route exact path="*" element={<Components.Error404 />} />
-        </Routes>
-      </BrowserRouter>
+        <Route exact path="*" element={<Components.Error404 />} />
+      </Routes>
     </div>
   );
 }
