3c55da3c35da627fe2f14af1cb9d025f3babe8d4
betodev <aaayza@gmail.com>
2023-10-24 20:08:00 -0300

    Frontend Development Libraries Certification added

--- src/pages/achievements/Achievements.jsx
+++ src/pages/achievements/Achievements.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React from "react";
 
 export const Achievements = () => {
-    return (
-        <div className='text-center container'>
-            <h1>Achievements</h1>
-            <div className="bg-dark d-inline-block p-2 rounded border border-3 border-success">
-            <ul className="list-unstyled">
-                <li>
-                    <a href="https://academy.hackthebox.com/achievement/845032/18">Linux Fundamentals</a>
-                </li>
-                <li>
-                    <a href="https://academy.hackthebox.com/achievement/845032/39">Using Metasploit Framework</a>
-                </li>
-                <li>
-                    <a href="https://academy.hackthebox.com/achievement/845032/75">Introduction to Web Applications</a>
-                </li>
-                <li>
-                    <a href="https://academy.hackthebox.com/achievement/845032/34">Introduction to Networking</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="text-center container">
+      <h1>Achievements</h1>
+      <div className="bg-dark d-inline-block p-2 rounded border border-3 border-success">
+        <h3>Cibersecurity</h3>
+        <ul className="list-unstyled">
+          <li>
+            <a href="https://academy.hackthebox.com/achievement/845032/18">
+              Linux Fundamentals
+            </a>
+          </li>
+          <li>
+            <a href="https://academy.hackthebox.com/achievement/845032/39">
+              Using Metasploit Framework
+            </a>
+          </li>
+          <li>
+            <a href="https://academy.hackthebox.com/achievement/845032/75">
+              Introduction to Web Applications
+            </a>
+          </li>
+          <li>
+            <a href="https://academy.hackthebox.com/achievement/845032/34">
+              Introduction to Networking
+            </a>
+          </li>
+        </ul>
+
+        <h3>Web development</h3>
+        <ul className="list-unstyled">
+          <li>
+            <a href="https://www.freecodecamp.org/certification/betoayza/front-end-development-libraries">
+              Frontend Development Libraries
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
